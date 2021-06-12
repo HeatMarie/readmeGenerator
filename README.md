2c4770af083200c6e83822c553345dc7a18b82ac
Heather Bjoin <h.m.bjoin@gmail.com>
2021-06-12 18:04:44 -0500

    Adding in all the things becausse I forgot to

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# readmeGenerator
-Making my life easier with a README Generator. ;) 
+
+  # no 
+  ![Badge for Github repo](https://img.shields.io/github/license/HeatMarie/readmeGenerator?color=%23d1730b)
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  
+  ## Description
+  no
+
+  ## Installation
+  no
+
+  ## Usage
+  no
+
+  ## License
+  GNU General Public License v3.0
+
--- index.js
+++ index.js
@@ -1,4 +1,68 @@
+// Packages need for this application
+
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+const generateMarkdown = require("./utils/markdown");
+
+
+const writeFileAsync = util.promisify(fs.writeFile)
+
+
+function init() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: "userName",
+            message: 'Enter your Github username.'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: `Enter the project's name`,
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a description for the project.',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter the installation instructions.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information.'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense' ]
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter the contributing guidelines.'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What tests have been written for this application?'
+        }
+    ])
+        .then((response) => writeFileAsync('README.md', generateMarkdown(response)))
+        .then(() => console.log('Successfuly wrote to README.md'))
+        .catch((err) => console.log(err));
+}
+
+
+init();
+
 
