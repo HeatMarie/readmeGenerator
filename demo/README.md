10ffce0bf4923e727c47cb64f9567b0441fb6d3a
Heather Bjoin <h.m.bjoin@gmail.com>
2021-06-13 20:37:45 -0500

    Adding license information

--- demo/README.md
+++ demo/README.md
@@ -1,11 +1,11 @@
 
   # 
 
-  ![badge](https://img.shields.io/static/v1?label=license&message=GNU%20General%20Public%20License%20v3.0&color=orange)
+  ![badge](https://img.shields.io/static/v1?label=license&message=BSD%202-Clause%20"Simplified"%20License&color=orange)
 
-  ![Github commit activity](https://img.shields.io/github/commit-activity/m/heatMarie/?color=%235FCE3B&style=for-the-badge)
+  ![Github commit activity](https://img.shields.io/github/commit-activity/m//?color=%235FCE3B&style=for-the-badge)
 
-  ![Github top language](https://img.shields.io/github/languages/top/heatMarie/?color=%23AF630D&style=for-the-badge)
+  ![Github top language](https://img.shields.io/github/languages/top//?color=%23AF630D&style=for-the-badge)
 
   ## Table of Contents
   * [Description](#description)
@@ -16,7 +16,7 @@
   
 
   ## Live URL
-  <a href="https://heatMarie.github.io//"> Click for live URL </a>
+  <a href="https://.github.io//"> Click for live URL </a>
 
   ## Description
   ()
@@ -28,13 +28,33 @@
   
 
   ## License
-  GNU General Public License v3.0
+  BSD 2-Clause "Simplified" License<br>
+  Redistribution and use in source and binary forms, with or without
+      modification, are permitted provided that the following conditions
+      are met:
+      1. Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+      
+      THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS
+      OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+      WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+      ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY 
+      DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+      DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+      OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+      HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+      LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+      OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+      SUCH DAMAGE. 
 
   ## Questions
   ### For any questions or comments feel free to contact me with the email below.
 
   <a href="mailto:">Email</a>
 
-  <a href="https://github.com/heatMarie">Github</a>
+  <a href="https://github.com/">Github</a>
   
 
--- utils/markdown.js
+++ utils/markdown.js
@@ -12,10 +12,70 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  let license;
+  let license = data.license;
   if(data.license.includes('%20')){
     license = data.license.replaceAll("%20", " ");
+   };
+   
+   let licenseDescription = license;
+
+   switch(licenseDescription){
+    case 'Apache 2.0':
+      licenseDescription =`<a href="https://choosealicense.com/licenses/apache-2.0/">Apache 2.0 Description</a>`;
+      break;
+    case 'GNU General Public License v3.0':
+      licenseDescription = `<a href="https://choosealicense.com/licenses/gpl-3.0/">General Public License v3.0 Description</a>`;
+      break;
+    case 'MIT':
+      licenseDescription = `
+                MIT License
+
+          Copyright (c) 
+
+          Permission is hereby granted, free of charge, to any person obtaining a copy
+          of this software and associated documentation files (the "Software"), to deal
+          in the Software without restriction, including without limitation the rights
+          to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+          copies of the Software, and to permit persons to whom the Software is
+          furnished to do so, subject to the following conditions:
+
+          The above copyright notice and this permission notice shall be included in all
+          copies or substantial portions of the Software.
+
+          THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+          IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+          FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+          AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+          LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+          OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+          SOFTWARE.
+          `;
+       break;
+    case 'BSD 2-Clause "Simplified" License':
+      licenseDescription = `Redistribution and use in source and binary forms, with or without
+      modification, are permitted provided that the following conditions
+      are met:
+      1. Redistributions of source code must retain the above copyright
+         notice, this list of conditions and the following disclaimer.
+      2. Redistributions in binary form must reproduce the above copyright
+         notice, this list of conditions and the following disclaimer in the
+         documentation and/or other materials provided with the distribution.
+      
+      THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS
+      OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+      WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+      ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY 
+      DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+      DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+      OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+      HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+      LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+      OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+      SUCH DAMAGE. `
+      break;
    }
+
+
   return `
   # ${data.title}
 
@@ -46,7 +106,8 @@ function generateMarkdown(data) {
   ${data.usage}
 
   ## License
-  ${license}
+  ${license}<br>
+  ${licenseDescription}
 
   ## Questions
   ### For any questions or comments feel free to contact me with the email below.
